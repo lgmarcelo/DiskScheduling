db9493a1a53a52e84ee6f69e7e64685690494e44
walbagg <lrnmrcl13@gmail.com>
2023-05-17 23:07:15 +0800

    add cscan algorithm
    plotting cscan not yet correct

--- src/Main.java
+++ src/Main.java
@@ -43,10 +43,10 @@ public class Main {
                 locationArr[i] = sc.nextInt();
             }
 
-            int[] arr = new int[numReq+1];
+            int[] arr = new int[numReq + 1];
             arr[0] = currentPosition;
-            for(int i = 1; i < arr.length; i++){
-                arr[i] = locationArr[i-1];
+            for (int i = 1; i < arr.length; i++) {
+                arr[i] = locationArr[i - 1];
             }
             do {
                 System.out.print("\n[A] First Come First Serve (FCFS)" +
@@ -174,7 +174,7 @@ public class Main {
         frame.pack();
         frame.setVisible(true);
     }
-    
+
     public static void SSTF(int curPos, int trkSz, int[] arr) {
         int[] visited = new int[arr.length];  // Array to store the visited locations
         int totalHeadMovement = 0;
@@ -272,7 +272,87 @@ public class Main {
     }
 
     public static void CSCAN(int curPos, int trkSz, int[] arr) {
+        int totalHeadMovement = 0;
+        int maxTrack = trkSz - 1;  // Maximum track number
 
+        // Sort the request locations in ascending order
+        Arrays.sort(arr);
+
+        // Find the index where the current position is less than or equal to the location
+        int index = 0;
+        while (index < arr.length && arr[index] < curPos) {
+            index++;
+        }
+
+        // Calculate head movement for the right direction
+        for (int i = index; i < arr.length; i++) {
+            totalHeadMovement += Math.abs(curPos - arr[i]);
+            curPos = arr[i];
+        }
+
+        // Move the head to the maximum track
+        totalHeadMovement += Math.abs(curPos - maxTrack);
+        curPos = maxTrack;
+
+        // Move the head to the starting track (track 0)
+        totalHeadMovement += Math.abs(curPos - 0);
+        curPos = 0;
+
+        // Calculate head movement for the left direction
+        for (int i = 0; i < index; i++) {
+            totalHeadMovement += Math.abs(curPos - arr[i]);
+            curPos = arr[i];
+        }
+
+        System.out.println("Total Head Movement: " + totalHeadMovement);
+
+        // Create a new array to represent the correct order of head movement
+        int[] headMovement = new int[arr.length];
+        headMovement[0] = curPos;
+        for (int i = 1; i < headMovement.length; i++) {
+            headMovement[i] = arr[i];
+        }
+
+// Generate data for line plot
+        double[][] data = new double[2][headMovement.length];
+
+// Set data points for head movement
+        for (int i = 0; i < headMovement.length; i++) {
+            data[0][i] = headMovement[i];
+            data[1][i] = i;
+        }
+
+        DefaultXYDataset dataset = new DefaultXYDataset();
+        dataset.addSeries("Head Movement", data);
+
+        // Create line plot
+        JFreeChart chart = ChartFactory.createXYLineChart(
+                "C-SCAN Disk Scheduling", "", "", dataset);  // Swap the x-axis and y-axis labels
+
+        XYPlot plot = chart.getXYPlot();
+        plot.getRenderer().setSeriesPaint(0, Color.BLUE);
+        plot.getRenderer().setSeriesStroke(0, new BasicStroke(2.0f));
+
+        // Customize x-axis to display whole numbers and place it on the top
+        NumberAxis domainAxis = (NumberAxis) plot.getDomainAxis();
+        domainAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+        domainAxis.setTickMarkInsideLength(4);  // Adjust the tick mark length
+        domainAxis.setAxisLineVisible(false);  // Hide the x-axis line
+
+        // Move the x-axis labels to the top
+        plot.setDomainAxisLocation(AxisLocation.TOP_OR_RIGHT);
+
+        // Customize y-axis to display whole numbers, reverse the range, and place it on the left side
+        NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+        rangeAxis.setTickMarkInsideLength(4);  // Adjust the tick mark length
+        rangeAxis.setAxisLineVisible(false);  // Hide the y-axis line
+        rangeAxis.setInverted(true);  // Reverse the range
+
+        // Display line plot
+        ChartFrame frame = new ChartFrame("Disk Scheduling Visualization", chart);
+        frame.pack();
+        frame.setVisible(true);
     }
 
     public static void CLOOK(int curPos, int trkSz, int[] arr) {
